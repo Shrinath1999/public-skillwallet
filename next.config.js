192b7fd9cca223753b455f9dfc57746b62c20542
Shrinath Malavekar <shrinath.m@codewalla.com>
2026-03-25 17:13:31 +0530

    Add basePath and assetPrefix for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/public-skillwallet',
+  assetPrefix: '/public-skillwallet',
   images: {
     unoptimized: true,
     remotePatterns: [
